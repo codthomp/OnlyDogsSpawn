7c9599c8e55bc4e2d38fe5258c1495b77883a4c7
Cody Thompson <cody3thomp@gmail.com>
2023-12-02 21:31:59 -0800

    Everything compiled in VS

---
+++ OnlyDogsSpawn/Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace OnlyDogsSpawn
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class OnlyDogsSpawn : BaseUnityPlugin
+    {
+        public static bool loaded;
+        private const string modGUID = "Alphonso.OnlyDogsSpawn";
+        private const string modName = "OnlyDogsSpawn";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static OnlyDogsSpawn Instance;
+        public static ManualLogSource mls;
+        private void Awake()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource("OnlyDogsSpawn");
+            // Plugin startup logic
+            mls.LogInfo("Loaded OnlyDogsSpawn and applying patches.");
+            harmony.PatchAll(typeof(OnlyDogsSpawn));
+            mls = Logger;
+        }
+
+        [HarmonyPatch(typeof(RoundManager), nameof(RoundManager.LoadNewLevel))]
+        [HarmonyPrefix]
+        static bool ModifyLevel(ref SelectableLevel newLevel)
+        {
+            HUDManager.Instance.AddTextToChatOnServer("<color=red>All Outside enemies are dogs</color>");
+
+            foreach (var item in newLevel.OutsideEnemies)
+            {
+                item.rarity = 0;
+                if (item.enemyType.enemyPrefab.GetComponent<MouthDogAI>() != null)
+                {
+                    item.rarity = 999;
+                }
+            }
+            return true;
+        }
+
+    }
+}
